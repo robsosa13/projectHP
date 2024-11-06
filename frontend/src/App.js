231905b098e16ee82bee01da99fd1b0a2e051ee5
rsosa <rob.sosa13@gmail.com>
2024-11-05 20:09:37 -0400

    Testeo

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.jsx";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+        <h1>
+          sda
+          </h1>
+        </Container>
+      </main>
+    </>
+  );
+};
+export default App;
